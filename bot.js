db1f0e886443402d5d235f7cf3a1e0e449e01272
Sukasa <nick.m@shaw.ca>
2020-02-17 21:03:26 -0800

    Create rewrite branch

--- bot.js
+++ bot.js
@@ -212,6 +212,9 @@ bot.on("messageCreate", async function (msg) {
 					break;
 				}
 			}
+			if (!replace[0]) {
+				// TODO check if we have a default set for the server
+			}
 		}
 			
 		if(replace[0]) {
@@ -426,10 +429,11 @@ bot.cmds = {
 	
 	form: {
 		help: cfg => "Configure alternate forms for a tulpa (overrides `" + cfg.prefix + "avatar`)",
-		usage: cfg =>  [ "form <name> <formName> - Switch a " + cfg.lang + "'s form",
-			"form <name> add <formName> <formUrl> - Add a new named form to a " + cfg.lang + " (4 max)", 
-			"form <name> remove <formName> - Remove a named form from a " + cfg.lang + ".  Cannot remove their last form",
-			"form <name> rename <oldName> <newName> - Rename a form"],
+		usage: cfg =>  [ "form[s] <name> <formName> - Switch a " + cfg.lang + "'s form",
+			"form[s] <name> add <formName> <formUrl> - Add a new named form to a " + cfg.lang + " (4 max)", 
+			"form[s] <name> remove <formName> - Remove a named form from a " + cfg.lang + ".  Cannot remove their last form",
+			"form[s] <name> rename <oldName> <newName> - Rename a form"],
+		aliases: ["forms"],
 		permitted: () => true,
 		execute: function(msg, args, cfg) {
 			let out = "";
@@ -589,6 +593,38 @@ bot.cmds = {
 			send(msg.channel, out);
 		}
 	},
+	
+	/*default: {
+		help: cfg => "Configure a default " + cfg.lang + " to use on this server if no prefixes are included",
+		usage: cfg =>  ["default <name> - Set the named " + cfg.lang + " as default for this server",
+						"default clear - Unset the default " + cfg.lang + " as default for this server"],
+		permitted: () => true,
+		execute: function(msg, args, cfg) {
+			let out = "";
+			args = getMatches(msg.content,/['](.*?)[']|(\S+)/gi).slice(1);
+			let name = args.join(" ");
+			if(!args[0]) {
+				return bot.cmds.help.execute(msg, ["default"], cfg);
+			} else if (args[0].toLowerCase() == "clear") {
+				out = "Default " + cfg.lang + " cleared.";
+			} else if(!tulpae[msg.author.id]) {
+				out = "You do not have any " + cfg.lang + "s registered.";
+			} else if(!tulpae[msg.author.id].find(t => t.name.toLowerCase() == name.toLowerCase())) {
+				out = "Could not find " + cfg.lang + " with that name registered under your account.";
+			} else {
+				out = proper(cfg.lang) + " set as default.";
+				let arr = tulpae[msg.author.id];
+				
+				let tul = arr.find(t => t.name.toLowerCase() == name.toLowerCase());
+				Object.keys(config).filter(t => config[t].rolesEnabled && bot.guilds.has(t)).map(t => bot.guilds.get(t)).forEach(g => {
+					if(tul.roles && tul.roles[g.id]) g.deleteRole(tul.roles[g.id]);
+				});
+				arr.splice(arr.indexOf(tul), 1);
+				save("tulpae",tulpae);
+			}
+			send(msg.channel, out);
+		}
+	}*/
 	
 	remove: {
 		help: cfg => "Unregister a " + cfg.lang + "",
