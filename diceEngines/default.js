b2557abe3a3593626fa785250adc4b768d443008
Sukasa <nick.m@shaw.ca>
2020-03-08 22:13:11 -0700

    Update dice system to support multiple dice engines in future

---
+++ diceEngines/default.js
@@ -0,0 +1,115 @@
+let diceRegex = /!(?:(?:roll\s|dice\s)?((?:\d+)?)d(\d+))\s*((?:\s*?(?:-|\+|top|bottom|drop|tn)\s*-?\d+\s*?)+)?/gi;
+let modifierRegex = /(?:(\+|\-|top|bottom|drop|tn)\s*(\d+))/gi;
+const crypto = require("crypto");
+
+module.exports = function () {
+
+	// Should test the message and return any matches, to be passed to execute()
+	function test(msg) {
+		let resultSets = [];
+
+		while (match = diceRegex.exec(msg.content))
+			resultSets.push(match);
+
+		return resultSets[0] && resultSets;
+	}
+
+	function doRoll(rollMatch, engineCfg) {
+		let numDice = rollMatch[1] || 1;
+		let numSides = rollMatch[2];
+		let modifiers = rollMatch[3];
+		let finalAdjust = 0;
+		let tn;
+
+		let rolls = [];
+
+		let bytes = Math.floor(Math.log2(numSides - 1) / 8) + 1;
+		let cap = Math.pow(2, bytes * 8) - (Math.pow(2, bytes * 8) % numSides);
+		let sanity = 0;
+		do {
+			let rollsRaw = crypto.randomBytes(numDice * bytes);
+			if (++sanity > 10)
+				throw "Failed to generate dice rolls, please try again";
+
+			for (var d = numDice - 1; d >= 0; d--) {
+				let rollBytes = rollsRaw.slice(d * bytes, d * bytes + bytes + 1);
+				let rollValue = rollBytes.reduce((a, b) => a * 256 + b);
+				if (rollValue % numSides < cap)
+					rolls.unshift({ value: (rollValue % numSides) + 1, valid: true });
+			}
+		} while (rolls.length < numDice);
+
+		rolls = rolls.slice(0, numDice);
+
+		if (modifiers != null) {
+			var modifier;
+			while (modifier = modifierRegex.exec(modifiers)) {
+				// Apply modifier
+				var by = modifier[2];
+				var indice = 0;
+				switch (modifier[1]) {
+					case "top":
+						by = numDice - by;
+					case "drop":
+						for (var passes = 0; passes < by; passes++) {
+							indice = rolls.reduce((iMin, x, i, arr) => x.value < arr[iMin].value && x.valid ? i : iMin, 0)
+							rolls[indice].valid = false;
+						}
+						break;
+					case "bottom":
+						by = numDice - by;
+						for (var passes = 0; passes < by; passes++) {
+							indice = rolls.reduce((iMax, x, i, arr) => x.value > arr[iMax].value && x.valid ? i : iMax, 0)
+							rolls[indice].valid = false;
+						}
+						break;
+					case "tn":
+						tn = by;
+						break;
+					case "+":
+						finalAdjust = Number(finalAdjust) + Number(by);
+						break;
+					case "-":
+						finalAdjust = Number(finalAdjust) - Number(by);
+						break;
+				}
+			}
+		}
+
+		// Modifiers applied and final adjustment done.  Now to make the output string.
+		var finalResult = rolls.map(x => x.valid ? `${x.value}` : `||${x.value}||`).join(", ");
+		var output = `**${numDice}d${numSides}${modifiers ? ` ${modifiers}` : ""}**: ${finalResult}`;
+		let finalRoll = rolls.reduce((s, r) => s + (r.valid && r.value), 0);
+
+		if (tn) {
+			tn = Number(tn) + Number(finalAdjust);
+			var degrees = 0;
+
+			if (engineCfg.tnstyle == "tens") {
+				degrees = Math.abs(Math.floor(tn / 10) - Math.floor(finalRoll / 10)) + 1;
+			} else {
+				degrees = Math.floor(Math.abs(tn - Math.floor(finalRoll)) / 10) + 1;
+			}
+
+			return `${output} = ** ${finalRoll}** (TN = ${tn}).**  ${finalRoll <= tn ? "Pass" : "Fail"}** by **${degrees}** degree${(degrees > 1 ? "s" : "")}`;
+		} else {
+			if (finalAdjust > 0)
+				output = `${output} + ${finalAdjust}`;
+			if (finalAdjust < 0)
+				output = `${output} - ${Math.abs(finalAdjust)}`;
+
+			return `${output} = **${finalRoll + finalAdjust}**`; 
+		}
+	}
+
+	function execute(state, cfg) {
+		return state.map(x => doRoll(x, cfg));
+	}
+
+	return {
+		test: test,
+		execute: execute,
+		settings: {"tnstyle": ["default","tens"]}
+	};
+
+}
--- listeners/messageCreateHandlers/diceHandler.js
+++ listeners/messageCreateHandlers/diceHandler.js
@@ -1,107 +1,22 @@
-const priorities = require("../component/priorities");
-let diceRegex = /!(?:(?:roll\s|dice\s)?((?:\d+)?)d(\d+))\s*((?:\s*?(?:-|\+|top|bottom|drop)\s*-?\d+\s*?)+)?/gi;
-let modifierRegex = /(?:(\+|\-|top|bottom|drop)\s*(\d+))/gi;
-
-
-function reduceDice(state, dice) {
-	if (dice.valid) {
-		state.content = `${state.content}${state.sep}${dice.value}`;
-		state.sum += dice.value;
-	} else
-		state.content = `${state.content}${state.sep}||${dice.value}||`;
-	state.sep = ", ";
-	return state;
-}
-
-function handleDiceRoll(rollMatch) {
-	var numDice = rollMatch[1] || 1;
-	var numSides = rollMatch[2];
-	var modifiers = rollMatch[3];
-	var finalAdjust = 0;
-
-	var rolls = [];
-	for (var d = numDice; d > 0; d--)
-		rolls.push({ value: Math.floor(Math.random() * numSides) + 1, valid: true });
-
-	if (modifiers != null) {
-		var modifier;
-		while (modifier = modifierRegex.exec(modifiers)) {
-			// Apply modifier
-			var by = modifier[2];
-			var last;
-			var indice = 0;
-			switch (modifier[1]) {
-				case "top":
-					by = numDice - by;
-				case "drop":
-					for (var passes = 0; passes < by; passes++) {
-						last = numSides + 1;
-						for (var test = 0; test < numDice; test++) {
-							var roll = rolls[test];
-							if (last > roll.value && roll.valid) {
-								indice = test;
-								last = roll.value;
-							}
-						}
-						rolls[indice].valid = false;
-					}
-					break;
-				case "bottom":
-					by = numDice - by;
-					for (var passes = 0; passes < by; passes++) {
-						last = 0;
-						for (var test = 0; test < numDice; test++) {
-							var roll = rolls[test];
-							if (last < roll.value && roll.valid) {
-								indice = test;
-								last = roll.value;
-							}
-						}
-						rolls[indice].valid = false;
-					}
-					break;
-				case "+":
-					finalAdjust = Number(finalAdjust) + Number(by);
-					break;
-				case "-":
-					finalAdjust = Number(finalAdjust) - Number(by);
-					break;
-			}
-		}
-	}
-	// Modifiers applied and final adjustment done.  Now to make the output string.
-
-	var finalResult = rolls.reduce(reduceDice, { content: "", sep: "", sum: 0 });
-
-	var output = "**" + numDice + "d" + numSides + (modifiers ? " " + modifiers : "") + "**: " + finalResult.content;
-	if (finalAdjust > 0)
-		output = output + " + " + finalAdjust;
-	if (finalAdjust < 0)
-		output = output + " - " + Math.abs(finalAdjust);
-
-	return output + " = **" + (finalResult.sum + finalAdjust) + "**";
-}
-
-
 module.exports = function (bot) {
+	const priorities = bot.priorities;
 
 	return {
 		priority: priorities.MEDIUM,
 
-		test: function (msg, bot) {
-			let cfg = msg.channel.guild && bot.config[msg.channel.guild.id] || { prefix: "t!", rolesEnabled: false, lang: "tulpa" };
-			return (diceRegex.test(msg.content) && (!cfg.diceblacklist || !cfg.diceblacklist.includes(msg.channel.id)));
+		test: function (msg) {
+			let cfg = bot.configuration.getServerConfig(msg);
+			let engine = bot.diceEngines[cfg.dice];
+			return engine && (!cfg.diceblacklist || !cfg.diceblacklist.includes(msg.channel.id)) && engine.test(msg, cfg.diceCfg);
 		},
 
-		execute: async (msg, bot) => {
-			diceRegex.lastIndex = 0;
-			var resultSets = [];
-			var match;
+		execute: async (msg, state) => {
 
-			while (match = diceRegex.exec(msg.content))
-				resultSets.push(handleDiceRoll(match));
+			let cfg = bot.configuration.getServerConfig(msg);
+			let engine = bot.diceEngines[cfg.dice];
+			let resultSets = engine.execute(state, cfg.diceCfg);
 
-			const hook = await bot.fetchWebhook(msg.channel)
+			const hook = await bot.webhooks.fetchWebhook(msg.channel)
 			const data = {
 				wait: true,
 				content: resultSets.join("\n"),
@@ -114,8 +29,8 @@ module.exports = function (bot) {
 			} catch (e) {
 				console.log(e);
 				if (e.code === 10015) {
-					delete webhooks[msg.channel.id];
-					const hook = await bot.fetchWebhook(msg.channel);
+					delete bot.serverWebhooks[msg.channel.id];
+					const hook = await bot.webhooks.fetchWebhook(msg.channel);
 					return bot.executeWebhook(hook.id, hook.token, data);
 				}
 			}
@@ -123,4 +38,4 @@ module.exports = function (bot) {
 			return;
 		}
 	};
-}]
+}
