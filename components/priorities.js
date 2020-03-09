2a411979d3ee850b4135e2cfcea48b90f43588ac
Sukasa <nick.m@shaw.ca>
2020-03-08 22:11:20 -0700

    Project structure cleanup
    Move some files around, clean up the packages, and update the gitignore and project file

---
+++ components/priorities.js
@@ -0,0 +1,8 @@
+module.exports = function (bot) {
+    return {
+        NONE: 0,
+        LOWEST: 1,
+        MEDIUM: 5,
+        HIGHEST: 10,
+    };
+}
--- util/priorities.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    NONE: 0,
-    LOWEST: 1,
-    MEDIUM: 5,
-    HIGHEST: 10,
-}
