11a62d0615308e3a7cb422ae49f31763b26388b2
Sukasa <nick.m@shaw.ca>
2020-05-14 00:02:20 -0700

    Update bot polish & logging

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const logger = require("winston");
 const fs = require("fs");
 const util = require("util");
 
-
 // Configure logging
 logger.configure({
 	level: "debug",
@@ -28,9 +27,13 @@ bot.updateStatus = function (status) {
 }
 
 bot.launch = function () {
-
-	bot.status = `Doing botty things`;
 	bot.logger = logger;
+
+	console.log("");
+	logger.info("**********");
+	logger.info("Launch");
+	console.log("");
+
 	bot.disconnects = 0;
 	bot.paramRegex = /["`'](.*?)["`']|(\S+)/gi;
 
@@ -40,20 +43,15 @@ bot.launch = function () {
 	bot.tasks = {};
 	bot.taskData = {};
 
-
-	bot.currNotify = "";
-	bot.lastNotify = 0;
-
-
 	// Load configuration
 	fs.readdirSync("./config").forEach(file => {
-		console.log(`Loading config ${file}`);
+		logger.info(`Loading config ${file}`);
 		bot[file.slice(0, -5)] = JSON.parse(fs.readFileSync("./config/" + file, 'utf8'));
 	});
 
 	// Load components
 	fs.readdirSync("./components").forEach(file => {
-		console.log(`Loading component ${file}`);
+		logger.info(`Loading component ${file}`);
 		if (bot[file.slice(0, -3)])
 			throw `Unable to load component ${file} due to clobber`;
 		bot[file.slice(0, -3)] = require("./components/" + file)(bot);
@@ -61,13 +59,13 @@ bot.launch = function () {
 
 	// Load Eris event listeners
 	fs.readdirSync("./listeners").filter(x => x.endsWith(".js")).forEach(file => {
-		console.log(`Loading listener ${file}`);
+		logger.info(`Loading listener ${file}`);
 		bot.on(file.slice(0, -3), require("./listeners/" + file)(bot).exec)
 	});
 
 	// Load commands (w/ aliases)
 	fs.readdirSync("./commands").filter(x => x.endsWith(".js")).forEach(file => {
-		console.log(`Loading command ${file}`);
+		logger.info(`Loading command ${file}`);
 		var command = require("./commands/" + file)(bot);
 		bot.commands[file.slice(0, -3)] = command;
 		var aliases = command.aliases || [];
@@ -78,18 +76,20 @@ bot.launch = function () {
 
 	// Load dice engines
 	fs.readdirSync("./diceEngines").forEach(file => {
-		console.log(`Loading dice engine ${file}`);
+		logger.info(`Loading dice engine ${file}`);
 		bot.diceEngines[file.slice(0, -3)] = require("./diceEngines/" + file)(bot);
 	});
 
 	// Load Tasks
 	fs.readdirSync("./tasks").forEach(file => {
-		console.log(`Loading task ${file}`);
+		logger.info(`Loading task ${file}`);
 		bot.tasks[file.slice(0, -3)] = require("./tasks/" + file)(bot);
 	});
 
-	console.log("\n**********");
-	console.log("Starting bot\n");
+	console.log("");
+	logger.info("**********");
+	logger.info("Connecting to Discord");
+	console.log("");
 
 	bot.connect();
 }
@@ -97,4 +97,3 @@ bot.launch = function () {
 process.on("unhandledRejection", console.log);
 
 bot.launch();
-
