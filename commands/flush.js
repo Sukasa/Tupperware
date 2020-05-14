e26e1d231827a369e50a7e52dd97a610dada5998
Sukasa <nick.m@shaw.ca>
2020-05-13 19:59:41 -0700

    Add owner command to flush datastores to disk

---
+++ commands/flush.js
@@ -0,0 +1,11 @@
+module.exports = function (bot) {
+	return {
+		help: cfg => `Flush datastore to disk`,
+		usage: cfg => [`flush`],
+		desc: cfg => `(OWNER ONLY): FLush datastores to disk`,
+		permitted: () => false,
+		execute: function (msg, args, cfg) {
+			bot.configuration.doSaves();
+		}
+	};
+}
