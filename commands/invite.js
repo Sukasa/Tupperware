9a3d883dc9f5051481e8c62cd958ad5e1e0092d6
Sukasa <nick.m@shaw.ca>
2021-02-04 21:58:16 -0800

    Update perms

--- commands/invite.js
+++ commands/invite.js
@@ -3,6 +3,6 @@ module.exports = function (bot) {
 		help: () => "Get the bot's invite URL",
 		usage: () => ["invite - sends the bot's oauth2 URL in this channel"],
 		permitted: () => bot.config.inviteCode != null,
-		execute: () => `https://discordapp.com/api/oauth2/authorize?client_id=${bot.config.inviteCode}&permissions=805314560&scope=bot`
+		execute: () => `https://discordapp.com/api/oauth2/authorize?client_id=${bot.config.inviteCode}&permissions=805577792&scope=bot`
 	};
 }
